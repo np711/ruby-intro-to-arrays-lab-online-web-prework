637c54e61ee52c1c2405b153972057414d5ff650
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-06-07 14:50:00 +0000

    Automatically backed up by Learn

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,15 @@
 
-RSpec.configure do |config|
+require './lib/intro_to_arrays.rb'
+describe "working with arrays" do
 
-  def __
-    
+  context 'instantiating' do
+    describe "#instantiate_new_array" do
+      #the variables that begin with the '@' symbol are called instance variables. 
+      #We'll learn more about them when we get to object orientation. 
+      #For now, just know that creating an instance variable inside our describe block 
+      #means we can access that variable anywhere inside the block.
+      before(:each) do
+        @my_new_array = instantiate_new_array
   end
 
 end
